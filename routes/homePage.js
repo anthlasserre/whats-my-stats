7a7d937c27238084b5271ecf405980b377bdd4e6
aTewan <tewan33@gmail.com>
2018-05-02 17:35:44 +0200

    Ajout GET Rocket League

--- routes/homePage.js
+++ routes/homePage.js
@@ -3,6 +3,7 @@ const routeur = express.Router()
 
 const OW = require('../services/overwatch.js')
 const R6 = require('../services/R6.js')
+const RL = require('../services/rocketleague.js')
 
 routeur.get('/', async (req, res, next) =>{
     res.status(200).render('index', {pageTitle: "What's My Stats"}) 
@@ -18,6 +19,13 @@ routeur.get('/overwatch/:battletag', async(req,res,next) => {
 routeur.get('/R6/:username', async(req,res,next) => {
     res.status(200);
     R6.searchPlayer('uplay',req.params.username).then(function(data) {
+        console.log(data)
+    })
+})
+
+routeur.get('/RocketLeague/:username', async(req,res,next) => {
+    res.status(200);
+    RL.searchPlayer('steam',req.params.username).then(function(data) {
         console.log(data)
     })
 })
