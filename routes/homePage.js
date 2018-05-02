b735f5bc22ca206ae42ee8a37d2543e1d492bb40
aTewan <tewan33@gmail.com>
2018-05-02 15:25:49 +0200

    Ajout de la récupération de données pour le jeu Overwatch

--- routes/homePage.js
+++ routes/homePage.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const routeur = express.Router()
 
+const OW = require('../services/overwatch.js')
+
 routeur.get('/', async (req, res, next) =>{
     res.status(200).render('index', {pageTitle: "What's My Stats"}) 
+});
+
+routeur.get('/overwatch/:battletag', async(req,res,next) => {
+    res.status(200);
+    OW.searchPlayer('pc','eu',req.params.battletag).then(function(data) {
+        console.log(data)
+    });
 })
 
 module.exports = routeur
