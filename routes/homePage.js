9383980beb36adac5b07cfc67108bd7871764558
aTewan <tewan33@gmail.com>
2018-05-14 16:26:14 +0200

    Correction du merge

--- routes/homePage.js
+++ routes/homePage.js
@@ -1,15 +1,12 @@
+const OW = require('../services/overwatch.js')
+const R6 = require('../services/R6.js')
+const RL = require('../services/rocketleague.js')
 const Express = require('express')
 var routeur = Express.Router()
 const { search, home } = require('../controllers/games.js')
 
 routeur.get('/', home)
 routeur.post('/search', search)
-
-<<<<<<< HEAD
-=======
-const OW = require('../services/overwatch.js')
-const R6 = require('../services/R6.js')
-const RL = require('../services/rocketleague.js')
 
 routeur.get('/', async (req, res, next) =>{
     res.status(200).render('index', {pageTitle: "What's My Stats"}) 
@@ -35,6 +32,5 @@ routeur.get('/RocketLeague/:username', async(req,res,next) => {
         console.log(data)
     })
 })
->>>>>>> a2d9276c9f54c9bad2e3c94a8614c0a40badfd54
 
 module.exports = routeur
